5a3b29edde788b61e84eb53fc3194e40c9cfbcc3
ovimutali <ovimutali@gmail.com>
2022-06-06 14:00:01 -0700

    print tebaplha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * descripton print alphabet lowercase reverse orde
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
