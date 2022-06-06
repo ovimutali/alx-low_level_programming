92225b4cacba65280ae1c4c663cc4fa6dac41124
ovimutali <ovimutali@gmail.com>
2022-06-06 12:09:42 -0700

    print alhabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * descripton print alphabet lowercase
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
