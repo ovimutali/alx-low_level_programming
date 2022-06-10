ffde915722339a4deceeeac033e8a2881a7efda1
ovimutali <ovimutali@gmail.com>
2022-06-09 19:51:14 -0700

    printline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - print straight line on terminal
+ * @n: number of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n ; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
